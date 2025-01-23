c31a0ab1fca947f497e61c0d8629ad138df1280c
an-gaurigujar <gauri.g@applicationnexus.com>
2025-01-23 17:37:58 +0530

    Update Employee.php

--- Employee.php
+++ Employee.php
@@ -1,13 +1,10 @@
 <?php
 class Employee {
     private $conn;
-
-    // Constructor to initialize Database object
     public function __construct($db) {
         $this->conn = $db->conn;
     }
 
-    // Method to insert a new employee into the database
     public function addEmployee($name, $email, $birthdate) {
         $stmt = $this->conn->prepare("INSERT INTO employee (name, email, birthdate) VALUES (?, ?, ?)");
         $stmt->bind_param("sss", $name, $email, $birthdate);
@@ -18,8 +15,6 @@ class Employee {
             return false;
         }
     }
-
-    // Method to get all employees and calculate age
     public function getEmployees() {
         $sql = "SELECT name, email, birthdate, TIMESTAMPDIFF(YEAR, birthdate, CURDATE()) AS age FROM employee";
         $result = $this->conn->query($sql);
