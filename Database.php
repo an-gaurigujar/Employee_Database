7207ae0a4fa46a7fcd2a61c06c794a7e7f13a2a7
an-gaurigujar <gauri.g@applicationnexus.com>
2025-01-23 17:53:10 +0530

    Update Database.php

--- Database.php
+++ Database.php
@@ -8,12 +8,11 @@ class Database {
 
     public function __construct() {
         $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
-
+        
         if ($this->conn->connect_error) {
             die("Connection failed: " . $this->conn->connect_error);
         }
     }
-
     public function close() {
         $this->conn->close();
     }
